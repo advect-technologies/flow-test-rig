3a2c65116cdf80dc006ca8b7941b63a0123a534e
Daniel <51832539+dboe1776@users.noreply.github.com>
2026-05-07 23:58:02 -0700

    Refactor for daq tools (#2)
    * migrating to standard daq library
    
    * functional mqtt over  daq-tools
    
    * ability to throttle data forwarding
    
    * simplify config loader

--- machine.py
+++ machine.py
@@ -5,6 +5,16 @@ import time
 from dataclasses import asdict
 from loguru import logger
 from typing import Optional
+from daq_writer import DaqJsonlWriter
+from pathlib import Path
+from daq_tools import DAQIngestor
+
+class TerminateTaskGroup(Exception):
+    """Exception raised to terminate a task group."""
+
+async def force_terminate_task_group():
+    """Used to force termination of a task group."""
+    raise TerminateTaskGroup()
 
 async def event_handler(test_rig:TestRig,
                         event_q:asyncio.Queue,
@@ -58,6 +68,63 @@ async def event_handler(test_rig:TestRig,
                        
         except Exception as ex:
             logger.exception('Exception in event handler')
+
+async def flow_tasks(test_rig: TestRig,
+                     stop_flag: asyncio.Event,
+                     test_rig_event_q: asyncio.Queue,
+                     on_metrics_update = None,
+                     ):
+    print("Hello from st-test-rig!")
+    metrics_updated_flag = asyncio.Event()
+    daq_writer = DaqJsonlWriter(test_rig.config.daq)
+
+    async def update_metrics_loop():
+        while True:
+            await test_rig.update_metrics()
+            metrics_updated_flag.set()            
+            if on_metrics_update is not None:
+                on_metrics_update(test_rig.fetch_flat_metrics())
+            await asyncio.sleep(1)
+    
+    async def report_metrics_loop():
+        last_write = 0
+        while True:
+            await metrics_updated_flag.wait()
+            metrics_updated_flag.clear()
+            if time.time() - last_write > test_rig.config.daq.sample_period_s:
+                last_write = time.time()
+                await daq_writer.write(test_rig._metrics)                        
+
+    async def start_daq_ingestor():
+        if Path('daq_config.toml').exists():
+            config_path = Path('daq_config.toml')
+        else:
+            config_path = Path('default_daq_config.toml')        
+        try:
+            async with DAQIngestor.from_config_file(config_path) as ingestor:
+                logger.info("DAQIngestor started — watching for JSONL files")
+                await asyncio.Event().wait()  # run forever until cancelled
+        except Exception as e:
+            logger.error(f"DAQIngestor failed: {e}")
+
+    try:
+        async with asyncio.TaskGroup() as tg:
+            tg.create_task(update_metrics_loop())
+            tg.create_task(report_metrics_loop())
+            tg.create_task(event_handler(test_rig,test_rig_event_q))
+            tg.create_task(test_rig.do_supervisory_control(test_rig_event_q))
+            tg.create_task(start_daq_ingestor())
+            await stop_flag.wait()
+            await daq_writer.close()
+            tg.create_task(force_terminate_task_group())
+
+    except* TerminateTaskGroup:
+
+        logger.warning('All tasks stopped, shutting down')
+
+    except* Exception as eg:
+            # Optional: catch real errors from children
+            logger.error(f"Background tasks failed: {eg.exceptions}")
 
 class TestRig:
 
